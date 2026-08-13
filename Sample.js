a598050d0bb4be390c4e6ea0470f1d1bf09209d3
Yeabsira <yeabnegn11@gmail.com>
2026-08-13 11:10:06 +0300

    update

--- Sample.js
+++ Sample.js
@@ -1,4 +1,7 @@
-const add = (a,b) => { 
-    return a+b;
-}
-console.log(add(8,23));
+let nums =[1,2,3,4,5,6];
+let odds = nums.filter(n => n%3===0);
+let evens= nums.filter(n => n%2===0);
+let doubled=nums.map(n =>n *2);
+console.log(doubled);
+console.log(evens)
+console.log(odds);
