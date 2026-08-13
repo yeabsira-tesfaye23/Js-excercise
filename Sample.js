b5d2a3f070450d87c80d206e56162602b319f297
Yeabsira <yeabnegn11@gmail.com>
2026-08-13 11:16:50 +0300

    upd

--- Sample.js
+++ Sample.js
@@ -1,7 +1,8 @@
-let nums =[1,2,3,4,5,6];
-let odds = nums.filter(n => n%3===0);
-let evens= nums.filter(n => n%2===0);
-let doubled=nums.map(n =>n *2);
-console.log(doubled);
-console.log(evens)
-console.log(odds);
+let student = {
+  name: "Sara",
+  age: 19,
+  grade: "A"
+};
+
+console.log(student.name);
+console.log(student.age);
