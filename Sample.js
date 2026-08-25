b30f96e1ddf4ecffd0e18a0874e1d8582effc5f0
Yeabsira <yeabnegn11@gmail.com>
2026-08-25 20:35:26 +0300

    implement express server and student retrieval endpoint

--- Sample.js
+++ Sample.js
@@ -1,6 +1,32 @@
+const express = require("express");
+const cors = require("cors");
 
-localStorage.setItem("age", 20);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/students", (req, res) => {
+    const students =[
+        {
+            name
+        }
+    ]
+});
+res.status(200).json(students);
+app.listen(3000, () => {
+    console.log("The Server running");
+});
 
 
-let age = localStorage.getItem("age");
-console.log(typeof age);
+
+
+const getStudents = async () => {
+    const response = await fetch(
+        "http://localhost:3000/students"
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+};
