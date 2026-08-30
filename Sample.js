899cd660c5bdefc906b5cf6a2e163b99dd93f07a
Yeabsira <yeabnegn11@gmail.com>
2026-08-30 21:54:41 +0300

    update

--- Sample.js
+++ Sample.js
@@ -15,7 +15,7 @@ app.get("/students", (req, res) => {
 });
 res.status(200).json(students);
 app.listen(3000, () => {
-    console.log("Server running");
+    console.log("The Server running");
 });
 
 
